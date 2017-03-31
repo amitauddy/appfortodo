2c1296e3474f407a7c55bfb19fb1513b51b0f584
Amit Auddy <amitauddy94@gmail.com>
2017-04-01 02:37:28 +0530

    made some changes

--- todos.js
+++ todos.js
@@ -1,17 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+function getdate() {
+    var date = new Date();
+    return `${date.getHours()}:${date.getMinutes()} ${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
+}
+
 var todoItems = [
-    {id: 1, desc: 'Task 1', date: (new Date())},
-    {id: 2, desc: 'Task 2', date: (new Date())},
-    {id: 3, desc: 'Task 3', date: (new Date())},
+    {id: 1, desc: 'Task 1', date: (getdate())},
+    {id: 2, desc: 'Task 2', date: (getdate())},
+    {id: 3, desc: 'Task 3', date: (getdate())},
 ];
 
 router.get('/', function(req, res) {
     res.render('index', {
         title: "TODO LIST",
-        items: todoItems,
-        date: new Date()
+        items: todoItems
     });
 });
 
@@ -21,7 +25,7 @@ router.post('/add', function (req, res) {
         todoItems.push({
             id: todoItems.length + 1,
             desc: newItem,
-            date: new Date()
+            date: getdate()
         });
     }
     res.redirect('/');
